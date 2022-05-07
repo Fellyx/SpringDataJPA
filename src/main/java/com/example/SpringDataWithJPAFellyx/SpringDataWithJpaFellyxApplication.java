95f1a69054d1035e821040075c618d6dc87b6960
Fellyx <Fellyx@users.noreply.github.com>
2022-05-07 14:19:02 -0300

    Initializing Git

---
+++ src/main/java/com/example/SpringDataWithJPAFellyx/SpringDataWithJpaFellyxApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataWithJPAFellyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataWithJpaFellyxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataWithJpaFellyxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringDataWithJPAFellyx/SpringDataWithJpaFellyxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataWithJPAFellyx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataWithJpaFellyxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
