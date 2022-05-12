32ef4df52677affac9672d1d527e160d32a6e588
Fellyx <Fellyx@users.noreply.github.com>
2022-05-12 19:20:34 -0300

    Adding the packages in the main class.
    Adding the packages in the main class,
    starting the first configurations on the
    main class.

--- src/main/java/com/example/SpringDataWithJPAFellyx/SpringDataWithJpaFellyxApplication.java
+++ src/main/java/com/example/SpringDataWithJPAFellyx/SpringDataWithJpaFellyxApplication.java
@@ -1,13 +1,26 @@
 package com.example.SpringDataWithJPAFellyx;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
 public class SpringDataWithJpaFellyxApplication {
 
-	public static void main(String[] args) {
+	private static final Logger log = 
+	    LoggerFactory.getLogger(SpringDataWithJpaFellyxApplication.class);
+  
+	    public static void main(String[] args) {
 		SpringApplication.run(SpringDataWithJpaFellyxApplication.class, args);
+	}
+	    
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+	  return (args) -> {
+	    
+	  };
 	}
 
 }
