cb44c1ec73b759186863027dfc6082da30b83534
Fellyx <Fellyx@users.noreply.github.com>
2022-05-12 19:56:10 -0300

    Adding the returns of the main class.
    Addint the returns of the main class,
    with 4 main functions:
    * save a few customers
    * fetch all customers
    * fetch an individual customer by ID.
    * fetch customer by last name

--- src/main/java/com/example/SpringDataWithJPAFellyx/SpringDataWithJpaFellyxApplication.java
+++ src/main/java/com/example/SpringDataWithJPAFellyx/SpringDataWithJpaFellyxApplication.java
@@ -19,6 +19,35 @@ public class SpringDataWithJpaFellyxApplication {
 	@Bean
 	public CommandLineRunner demo(CustomerRepository repository) {
 	  return (args) -> {
+	    // save a few customers
+	    repository.save(new Customer("Jack", "Bauer"));
+	    repository.save(new Customer("Chloe", "O'Brian"));
+	    repository.save(new Customer("kim", "Bauer"));
+	    repository.save(new Customer("David", "Palmer"));
+	    repository.save(new Customer("Michelle", "Dessler"));
+	    
+	    // fetch all customers
+	    log.info("Customers found with findall():");
+	    log.info("--------------------------------");
+	    for (Customer customer : repository.findAll()) {
+	      log.info(customer.toString());
+	    }
+	    log.info("");
+	    
+	    // fetch an individual customer by ID
+	    Customer customer = repository.findById(1L);
+	    log.info("Customer found with findById(1l):");
+	    log.info("----------------------------------");
+	    log.info(customer.toString());
+	    log.info("");
+	    
+	    // fetch customers by last name
+	    log.info("Customer found with findByLastName('Bauer'):");
+	    log.info("---------------------------------------------");
+	    repository.findByLastName("Bauer").forEach(bauer -> {
+	      log.info(bauer.toString());
+	      log.info("");
+	    });
 	    
 	  };
 	}
